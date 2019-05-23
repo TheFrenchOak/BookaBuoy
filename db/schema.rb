71fefd999cd9900de830d2446e7ddcf8a3a90f9f
Frederic Laffont <frederic_laffont@msn.com>
2019-05-23 19:45:07 +0200

    add a description column to buoys

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_23_140945) do
+ActiveRecord::Schema.define(version: 2019_05_23_174043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2019_05_23_140945) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
     t.index ["user_id"], name: "index_buoys_on_user_id"
   end
 
