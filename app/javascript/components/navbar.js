f62011cc0e9e05b3d05e6abfaa6a9fbfd6947013
Myriam Chahlafi <myriam.chah@gmail.com>
2019-05-27 13:56:52 +0200

    logo on navbar and footer

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -5,11 +5,13 @@ const initUpdateNavbarOnScroll = () => {
 
   if (navbar) {
     window.addEventListener('scroll', () => {
-      if (window.scrollY >= window.innerHeight) {
+       if (window.scrollY >= window.innerHeight) {
         navbar.classList.add('navbar-lewagon-white');
         toggler.classList.remove('navbar-dark');
+        toggler.classList.add('navbar-light');
       } else {
         navbar.classList.remove('navbar-lewagon-white');
+        toggler.classList.remove('navbar-light');
         toggler.classList.add('navbar-dark');
       }
     });
