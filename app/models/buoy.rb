0d35b5bde653ef1c1aa6841590ad77d6c71aacc3
Frederic Laffont <frederic_laffont@msn.com>
2019-05-28 12:41:20 +0200

    operationnal search bar

--- app/models/buoy.rb
+++ app/models/buoy.rb
@@ -1,4 +1,10 @@
 class Buoy < ApplicationRecord
+  include PgSearch
+  pg_search_scope :search_by_name_description_and_address,
+    against: [ :name, :description, :address ],
+    using: {
+      tsearch: { prefix: true }
+    }
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
   mount_uploader :photo, PhotoUploader
